5b69ea9ada541c2878bfce83a9b4553304ca2429
omartoaba <42046875+omartoaba@users.noreply.github.com>
2023-08-18 17:30:44 +0100

    styling the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+
+import logo from '../../assets/ai.png'
 import './navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='ai__navbar'>
+      <div className="ai__navbar-links">
+        <div className="ai__navbar-links_logo">
+          <img src={logo}  alt='logo'/>
+        </div>
+        <div className="ai__navbar-links_container">
+            <p><a href='#home'>Home</a></p>
+            <p><a href='#ai'>AI</a></p>
+            <p><a href='#possibility'>Chat-GPT</a></p>
+            <p><a href='#home'>Features</a></p>
+            <p><a href='#blog'>Blog</a></p>
+        </div>
+        <div className="ai__navbar-sign">
+          <p>Sign in</p>
+          <button type='button'>Sign up</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
