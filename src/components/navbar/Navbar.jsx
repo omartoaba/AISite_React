063c5c914b86f004b2d0e05c0be53dc835db4f4a
omartoaba <42046875+omartoaba@users.noreply.github.com>
2023-08-19 18:01:23 +0100

    finishing working with navbar in all screens

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {RiCloseLine,RiMenu3Line} from 'react-icons/ri'
 import logo from '../../assets/ai.png'
 import './navbar.css'
+const Menu = () => {
+  return(
+  <>   
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#ai'>AI</a></p>
+    <p><a href='#possibility'>Chat-GPT</a></p>
+    <p><a href='#home'>Features</a></p>
+    <p><a href='#blog'>Blog</a></p>
+  </>)
+}
 const Navbar = () => {
+  const [toggleMenu,setToggleMenu] = useState(false); 
   return (
     <div className='ai__navbar'>
       <div className="ai__navbar-links">
@@ -10,17 +21,30 @@ const Navbar = () => {
           <img src={logo}  alt='logo'/>
         </div>
         <div className="ai__navbar-links_container">
-            <p><a href='#home'>Home</a></p>
-            <p><a href='#ai'>AI</a></p>
-            <p><a href='#possibility'>Chat-GPT</a></p>
-            <p><a href='#home'>Features</a></p>
-            <p><a href='#blog'>Blog</a></p>
-        </div>
-        <div className="ai__navbar-sign">
-          <p>Sign in</p>
-          <button type='button'>Sign up</button>
+            <Menu/>
         </div>
       </div>
+      <div className="ai__navbar-sign">
+          <p>Sign in</p>
+          <button type='button' >Sign up</button>
+      </div>
+      <div className="ai__navbar-menu">
+           {toggleMenu ? 
+              <RiCloseLine color='white' size={20} onClick={() => setToggleMenu(false)}/> :
+              <RiMenu3Line color='#fff' size={20} onClick={() => setToggleMenu(true)}/>}
+           {toggleMenu && (
+              <div className='ai__navbar-menu_container scale-up-center'>
+                <div className="ai__navbar-menu-links_container">
+                  <Menu/>
+                </div>
+                <div className="ai__navbar-menu_container-links-sign">
+                     <p>Sign in</p>
+                    <button type='button'>Sign up</button>
+                </div>
+              </div>
+           )
+           }
+        </div>
     </div>
   )
 }
