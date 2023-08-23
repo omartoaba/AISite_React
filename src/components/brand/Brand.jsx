a45b5324823a368c686b1e5eb65a12c1b698891b
omartoaba <42046875+omartoaba@users.noreply.github.com>
2023-08-23 12:20:33 +0100

    adding brands images

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { google,slack,shopify,atlassian,dropbox } from './imports';
 import './brand.css'
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='ai__brand section-padding'>
+      <div>
+        <img src={google} alt="Google"/>
+      </div>  
+      <div>
+        <img src={atlassian} alt="Atlassian" />
+      </div>
+      <div>
+        <img src={slack} alt="Slack" />
+      </div>
+      <div>
+        <img src={dropbox} alt="DropBox" />
+      </div>
+      <div>
+        <img src={shopify} alt="Shopify" />
+      </div>
+    </div>
   )
 }
 
