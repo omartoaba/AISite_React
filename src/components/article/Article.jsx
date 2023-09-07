79ee27b1f1d2ea247f89fe089dc3271b300c27d3
omartoaba <42046875+omartoaba@users.noreply.github.com>
2023-09-07 14:31:08 +0100

    finishin blog component

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './article.css'
-const Article = () => {
+const Article = ({imgUrl,date,title}) => {
   return (
-    <div>Article</div>
+    <div className='ai__blog-container_article'>
+      <div className="ai__blog-container_article-image">
+        <img src={imgUrl} alt="blogImage" />
+      </div>
+      <div className="ai__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read full article</p>
+      </div>
+   </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import Article from '../../components/article/Article'
+import './blog.css'
+import { blog01,blog02,blog03,blog04,blog05 } from './imports'
 function Blog() {
   return (
-    <div>blog</div>
+     <div className='ai__blog section__padding' id='blog'>
+        <div className="ai__blog-heading">
+          <h1 className="gradient__text">
+            A lot is happening, We are blogging about it
+          </h1>
+        </div>
+        <div className="ai__blog-container">
+          <div className="ai__blog-container-groupeA">
+            <Article imgUrl={blog01} date ="Sep 2,2023" title="let us explore what's AI"/>
+          </div>
+          <div className="ai__blog-container_groupeB">
+            <Article imgUrl={blog02} date ="Sep 2,2023" title="let us explore what's AI"/>
+            <Article imgUrl={blog03} date ="Sep 2,2023" title="let us explore what's AI"/>
+            <Article imgUrl={blog04} date ="Sep 2,2023" title="let us explore what's AI"/>
+            <Article imgUrl={blog05} date ="Sep 2,2023" title="let us explore what's AI"/>
+          </div>
+        </div>
+     </div>
   )
 }
 
